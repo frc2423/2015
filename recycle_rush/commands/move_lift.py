f6e5bbc1e0b34a1bc1cb25d4d0ecfee22c7eb8e5
Ghislaine Coulter-de Wit <Ghi@Ghislaines-MacBook-Air.local>
2015-02-04 21:02:36 -0500

    Updated move_lift. Should be functional now

--- recycle_rush/commands/move_lift.py
+++ recycle_rush/commands/move_lift.py
@@ -6,17 +6,32 @@
 from wpilib.command import Command
 from subsystems.grabber_lift import GrabberLift
 
-class RaiseLift(Command):
+class MoveLift(Command):
     
-    def __init__(self, grabber_lift):
+    def __init__(self, grabber_lift, param):
         '''
             Raises lift using manual control
             :param grabber_lift: subsystem of GrabberLift
+            Default command. We don't think we want it to be finished. EVER.
+        
+            :param value: value should be either a function that returns between -1 to 1 or a number between -1 to 1
         '''
         super().__init__()
         self.setInterruptible(True)
         self.grabber_lift = grabber_lift
         self.requires(GrabberLift)
+        
+        #
+        # checks if the passed parameter (param) is callable (read is a function)
+        # part 1. Continued in execute
+        #        
+        if callable(param):
+            self.param_is_callable = True
+        
+        else:
+            self.param_is_callable = False
+            
+        self.param = param
         
     def initialize(self):
         '''
@@ -28,5 +43,27 @@ class RaiseLift(Command):
         '''
             Called repeatedly when this Command is scheduled to run
         '''
-        self.grabber_lift.move_lifter()
+        #
+        # Part 2. Continued from __init__()
+        # if param is callable is, feeds move_lifter the appropriate thing (in execute)
+        # so if the passed param is a value it gives move_lifter a value
+        # otherwise it gives move_lifter the function. 
+        #
+        if self.param_is_callable:
+            self.grabber_lift.move_lifter(self.param())
+        else:
+            self.grabber_lift.move_lifter(self.param)
+        
+    def isFinished(self):
+        ''' Make this return true when this Command no longer needs to run execute()'''
+        return False
+    
+    def end(self):
+        pass
+    
+    def interrupted(self):
+        '''Called when another command which requires one or more of the same
+           subsystems is scheduled to run. Stops lift motor.'''
+        
+        self.grabber_lift.move_lifter(0)
         
